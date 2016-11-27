878fc39dda6569f5a4807c566df4c69a3578346b
pagegwood <page@pagegwood.com>
2016-11-27 12:51:03 -0500

    begin modern workflow for redesign: bedrock, trellis, and new theme scaffolding based on timber and sage

---
+++ site/web/app/themes/wood/lib/setup.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wood\Setup;
+
+use Wood\Assets;
+
+/**
+ * Theme setup
+ */
+function setup() {
+
+  load_theme_textdomain('wood', get_template_directory() . '/lang');
+
+  // Enable plugins to manage the document title
+  // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
+  add_theme_support('title-tag');
+
+  // Register wp_nav_menu() menus
+  // http://codex.wordpress.org/Function_Reference/register_nav_menus
+  register_nav_menus([
+    'primary_navigation' => __('Primary Navigation', 'wood')
+  ]);
+
+  // Enable post thumbnails
+  // http://codex.wordpress.org/Post_Thumbnails
+  // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+  // http://codex.wordpress.org/Function_Reference/add_image_size
+  add_theme_support('post-thumbnails');
+
+  // Enable post formats
+  // http://codex.wordpress.org/Post_Formats
+  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+
+  // Enable HTML5 markup support
+  // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
+  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+
+  // Use main stylesheet for visual editor
+  // To add custom styles edit /assets/styles/layouts/_tinymce.scss
+  add_editor_style(Assets\asset_path('styles/main.css'));
+}
+add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
+
+
+/**
+ * Theme assets
+ */
+function assets() {
+  wp_enqueue_style('wood/css', Assets\asset_path('styles/main.css'), false, null);
+
+  if (is_single() && comments_open() && get_option('thread_comments')) {
+    wp_enqueue_script('comment-reply');
+  }
+
+  wp_enqueue_script('wood/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
