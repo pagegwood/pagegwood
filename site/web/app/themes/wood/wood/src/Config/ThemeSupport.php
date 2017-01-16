1d150759a2376ec2f5bb1e1b7d38cc435f92145f
pagegwood <page@pagegwood.com>
2017-01-16 14:47:30 -0500

    Enable SVG uploads, ACF Export: break planks into cloned fields for better management

--- site/web/app/themes/wood/wood/src/Config/ThemeSupport.php
+++ site/web/app/themes/wood/wood/src/Config/ThemeSupport.php
@@ -9,5 +9,13 @@ class ThemeSupport
         add_theme_support('post-formats');
         add_theme_support('post-thumbnails');
         add_theme_support('menus');
+        add_filter( 'upload_mimes', [get_called_class(), 'addSvg']);
+    }
+
+    public static function addSvg()
+    {
+    	$mimes['svg'] = 'image/svg+xml';
+			// return the modified array
+			return $mimes;
     }
 }
