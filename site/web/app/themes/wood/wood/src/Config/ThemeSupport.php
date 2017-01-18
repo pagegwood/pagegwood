e3778e00441f1ca9246fd2840d64d03cd14cef95
pagegwood <page@pagegwood.com>
2017-01-18 07:49:51 -0500

    enable SVG uploads in WordPress

--- site/web/app/themes/wood/wood/src/Config/ThemeSupport.php
+++ site/web/app/themes/wood/wood/src/Config/ThemeSupport.php
@@ -9,10 +9,10 @@ class ThemeSupport
         add_theme_support('post-formats');
         add_theme_support('post-thumbnails');
         add_theme_support('menus');
-        //add_filter( 'upload_mimes', [get_called_class(), 'addSvg']);
+        add_filter( 'upload_mimes', [get_called_class(), 'addSvg']);
     }
 
-    public static function addSvg()
+    public static function addSvg($mimes)
     {
     	$mimes['svg'] = 'image/svg+xml';
 			// return the modified array
