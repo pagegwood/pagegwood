0fbbe1f4742c7eec3d7ad73a123348a22719a992
pagegwood <page@pagegwood.com>
2017-01-18 08:42:23 -0500

    blog archive UI and pagination

--- site/web/app/themes/wood/index.php
+++ site/web/app/themes/wood/index.php
@@ -19,9 +19,6 @@ if ( ! class_exists( 'Timber' ) ) {
 }
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
-$context['foo'] = 'bar';
+$context['pagination'] = Timber::get_pagination();
 $templates = array( 'index.twig' );
-if ( is_home() ) {
-	array_unshift( $templates, 'home.twig' );
-}
 Timber::render( $templates, $context );
