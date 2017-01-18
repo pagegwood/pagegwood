b3af8acda2c9cfd915632468579f0e28cbf7509f
pagegwood <page@pagegwood.com>
2017-01-17 20:39:14 -0500

    project teaser UI

--- site/web/app/themes/wood/wood/src/PostTypes/Project.php
+++ site/web/app/themes/wood/wood/src/PostTypes/Project.php
@@ -1,8 +1,39 @@
 <?php
 
 namespace Wood\PostTypes;
+use TimberImage;
 
 class Project extends Post
 {
-    protected static $postType = 'wood_project';
+
+	protected static $postType = 'wood_project';
+
+
+  public function previewImage()
+  {
+  	$id = get_field('wood_preview_image_id', $this->ID);
+
+    if (is_int($id)) {
+        return new TimberImage($id);
+    }
+  }
+
+  public function hasPreviewImage()
+  {
+    return !is_null($this->preview_image);
+  }
+
+  public function logoImage()
+  {
+  	$id = get_field('wood_logo_image_id', $this->ID);
+
+    if (is_int($id)) {
+        return new TimberImage($id);
+    }
+  }
+
+  public function hasLogoImage()
+  {
+    return !is_null($this->logo_image);
+  }
 }
