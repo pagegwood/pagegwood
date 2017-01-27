d5279bb17d233b7e0718b3944681440c3b48e9ef
pagegwood <page@pagegwood.com>
2017-01-26 21:49:34 -0500

    sticky animation, remove border from header

--- site/web/app/themes/wood/assets/scripts/ui/sticky_header.js
+++ site/web/app/themes/wood/assets/scripts/ui/sticky_header.js
@@ -4,7 +4,10 @@ export default class StickyHeader {
   static sticky() {
     const header = document.querySelector('.js-StickyHeader');
     if (header) {
-      const headroom = new Headroom(header);
+      const headroom = new Headroom(header, {
+        offset: 124,
+        tolerance: 0,
+      });
       // initialise
       headroom.init();
     }
