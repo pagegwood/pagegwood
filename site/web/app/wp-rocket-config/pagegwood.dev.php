2c62e341e29e2a021b8e5d80ee2118ab38b84a0d
pagegwood <page@pagegwood.com>
2017-07-02 13:54:54 -0400

    update wp-rocket

--- site/web/app/wp-rocket-config/pagegwood.dev.php
+++ site/web/app/wp-rocket-config/pagegwood.dev.php
@@ -4,7 +4,7 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 $rocket_cookie_hash = '946c159997d5a698b6989e5da1992dc4';
 $rocket_secret_cache_key = '589935ae7e604619186543';
 $rocket_cache_mobile = '1';
-$rocket_cache_reject_uri = '/wp-json/(.*)';
+$rocket_cache_reject_uri = '(.*)/feed/?|/wp-json/(.*)';
 $rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
 $rocket_cache_query_strings = array (
   0 => 'q',
