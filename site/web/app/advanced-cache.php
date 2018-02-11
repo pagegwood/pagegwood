0f3be6650c88fc2a429e40703e7dba67ddf89511
pagegwood <page@pagegwood.com>
2018-02-11 14:35:10 -0500

    upgrades: WP, acf, WP-rocket, wp smush, yoast, contact form 7, sendgrid

--- site/web/app/advanced-cache.php
+++ site/web/app/advanced-cache.php
@@ -6,7 +6,7 @@ $rocket_cache_path = '/srv/www/pagegwood.com/current/web/app/cache/wp-rocket/';
 $rocket_config_path = '/srv/www/pagegwood.com/current/web/app/wp-rocket-config/';
 
 if ( file_exists( '/srv/www/pagegwood.com/current/web/app/plugins/wp-rocket/inc/front/process.php' ) ) {
-	include( '/srv/www/pagegwood.com/current/web/app/plugins/wp-rocket/inc/front/process.php' );
+	include '/srv/www/pagegwood.com/current/web/app/plugins/wp-rocket/inc/front/process.php';
 } else {
 	define( 'WP_ROCKET_ADVANCED_CACHE_PROBLEM', true );
 }
